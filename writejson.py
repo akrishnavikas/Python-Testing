9ccd1bd47bae178c2238e3a830d968d9cb900cda
akrishnavikas <amidipuramkrishnavikas@gmail.com>
2025-09-24 21:40:44 +0530

    added a function in writejson python file

--- writejson.py
+++ writejson.py
@@ -5,3 +5,10 @@ with open("data.json","w") as file:
     json.dump(data,file)
 
 print("completed")
+
+def read_json():
+    with open("data.json","r") as file:
+        data = json.load(file)
+        print(data)
+
+read_json()
